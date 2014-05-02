803efa68161804aebb95799e5eb1572c9ebf02b9
xuwei-k <6b656e6a69@gmail.com>
2014-05-02 14:57:09 +0900

    use official giter8-plugin 0.6.4 instead of my fork

--- project/project/plugins.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-object PluginDef extends Build {
-  lazy val root = Project("plugins", file(".")) dependsOn(
-    ProjectRef(uri("git://github.com/xuwei-k/giter8#sbt0.13.0"), "giter8-plugin")
-  )
-}
